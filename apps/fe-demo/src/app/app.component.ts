9c26a5a3f08e1288e40e68d9eaedb33f5c37c0ce
Craig Shearer <craig@supacrash.com>
2023-05-27 16:44:54 +1200

    Upgrade to latest nx, Angular 16

--- apps/fe-demo/src/app/app.component.ts
+++ apps/fe-demo/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'rxjs-ws-demo-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+	selector: 'rxjs-ws-demo-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'fe-demo';
+	title = 'fe-demo';
 }
--- apps/fe-demo/src/app/app.module.ts
+++ apps/fe-demo/src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
-  providers: [],
-  bootstrap: [AppComponent],
+	declarations: [AppComponent, NxWelcomeComponent],
+	imports: [BrowserModule, BrowserAnimationsModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
 export class AppModule {}
